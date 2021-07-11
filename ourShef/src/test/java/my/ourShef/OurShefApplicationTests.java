646e0fa3c7ee00cac9d4916e9fb33c900dd2a8a4
jansegety <jansegety@gmail.com>
2021-07-11 21:11:33 +0900

    test commit message

---
+++ ourShef/src/test/java/my/ourShef/OurShefApplicationTests.java
@@ -0,0 +1,13 @@
+package my.ourShef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurShefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
